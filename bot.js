2a4a51bb7f853c3ac6521ae416c524c8b7253e96
Mohammad MohammadAlian <mralian.dev@gmail.com>
2020-06-03 17:42:41 +0430

    add /learnfulDate command (for admin) to show leanrful date :)

--- bot.js
+++ bot.js
@@ -17,8 +17,12 @@ if (process.env.NODE_ENV === 'production') {
 
 const updateUserLastActivityDate = async (message) => {
   await User.findOneAndUpdate(
-    { id: message.from.id },
-    { lastActivityDate: Date(message.date) }
+    {
+      id: message.from.id,
+    },
+    {
+      lastActivityDate: Date(message.date),
+    }
   );
 };
 
@@ -40,27 +44,39 @@ const getUserIds = async (message) => {
   users.forEach((user) => {
     usersListMessage = `${usersListMessage}\n<a href="tg://user?id=${user.id}">${user.id}</a>`;
   });
-  bot.sendMessage(message.chat.id, usersListMessage, { parse_mode: 'HTML' });
+  bot.sendMessage(message.chat.id, usersListMessage, {
+    parse_mode: 'HTML',
+  });
 };
 
 const getStatistics = async (message) => {
   if (message.chat.type != 'private') return;
 
-  const user = await User.findOne({ id: message.from.id });
+  const user = await User.findOne({
+    id: message.from.id,
+  });
   if (user === null)
     return bot.sendMessage(
       message.chat.id,
       'اطلاعاتی از شما در دیتابیس وجود ندارد',
-      { reply_to_message_id: message.message_id }
+      {
+        reply_to_message_id: message.message_id,
+      }
     );
 
   const lastActivityJalaliDate = moment
     .utc(user.lastActivityDate)
-    .add({ hours: 4, minutes: 30 })
+    .add({
+      hours: 4,
+      minutes: 30,
+    })
     .format('jYYYY/jMM/jDD HH:mm:ss');
   const joinJalaliDate = moment
     .utc(user.joinDate)
-    .add({ hours: 4, minutes: 30 })
+    .add({
+      hours: 4,
+      minutes: 30,
+    })
     .format('jYYYY/jMM/jDD HH:mm:ss');
 
   const numberOfLearnedThings = await Message.countDocuments({
@@ -79,7 +95,9 @@ const getStatistics = async (message) => {
 تاریخ عضویت: ${joinJalaliDate} 😘
 وضعیت در گروه: ${user.isActive ? 'فعال' : 'غیرفعال'}
     `,
-    { reply_to_message_id: message.message_id }
+    {
+      reply_to_message_id: message.message_id,
+    }
   );
 };
 
@@ -89,7 +107,11 @@ const getBestStudent = async (message) => {
     message.from.id != process.env.ADMIN_USER_ID
   )
     return;
-  let students = await Message.aggregate([{ $sortByCount: '$learnerId' }]);
+  let students = await Message.aggregate([
+    {
+      $sortByCount: '$learnerId',
+    },
+  ]);
   const bestStudent = students[0];
   bot.sendMessage(
     message.chat.id,
@@ -97,17 +119,18 @@ const getBestStudent = async (message) => {
   بهترین یادگیرنده: <a href="tg://user?id=${bestStudent._id}">${bestStudent._id}</a> ✌
   تعداد چیزهای یادگرفته شده: ${bestStudent.count} 😉
 `,
-    { parse_mode: 'HTML' }
+    {
+      parse_mode: 'HTML',
+    }
   );
 };
 
 const getBestTeacher = async (message) => {
-  if (
-    message.chat.type !== 'private' ||
-    message.from.id != process.env.ADMIN_USER_ID
-  )
-    return;
-  let teachers = await Message.aggregate([{ $sortByCount: '$senderId' }]);
+  let teachers = await Message.aggregate([
+    {
+      $sortByCount: '$senderId',
+    },
+  ]);
   const bestTeacher = teachers[0];
   bot.sendMessage(
     message.chat.id,
@@ -115,8 +138,53 @@ const getBestTeacher = async (message) => {
   بهترین یاددهنده: <a href="tg://user?id=${bestTeacher._id}">${bestTeacher._id}</a> ✌
   تعداد چیزهای یادداده شده: ${bestTeacher.count} 😉
 `,
-    { parse_mode: 'HTML' }
+    {
+      parse_mode: 'HTML',
+    }
   );
+};
+
+const getLearnfulDate = async (message) => {
+  if (
+    message.chat.type !== 'private' ||
+    message.from.id != process.env.ADMIN_USER_ID
+  )
+    return;
+  const messagesSortedByCountOfLearnDate = await Message.aggregate([
+    {
+      $project: {
+        year: {
+          $year: '$learnDate',
+        },
+        month: {
+          $month: '$learnDate',
+        },
+        day: {
+          $dayOfMonth: '$learnDate',
+        },
+      },
+    },
+    {
+      $project: {
+        date: {
+          year: '$year',
+          month: '$month',
+          day: '$day',
+        },
+      },
+    },
+    {
+      $sortByCount: '$date',
+    },
+  ]);
+  const learnFulDate = moment(
+    new Date(
+      messagesSortedByCountOfLearnDate[0]._id.year,
+      messagesSortedByCountOfLearnDate[0]._id.month,
+      messagesSortedByCountOfLearnDate[0]._id.day
+    )
+  );
+  bot.sendMessage(message.chat.id, learnFulDate.format('jYYYY/jMM/jDD'));
 };
 
 const onLearnedNewThing = async (message) => {
@@ -139,16 +207,28 @@ const onLearnedNewThing = async (message) => {
     id: message.message_id,
     senderId: message.reply_to_message.from.id,
     learnerId: message.from.id,
-    learnDate: message.date,
+    learnDate: Date(message.date),
   });
 
   await User.findOneAndUpdate(
-    { id: message.from.id },
-    { $inc: { numberOfLearnedThings: 1 } }
+    {
+      id: message.from.id,
+    },
+    {
+      $inc: {
+        numberOfLearnedThings: 1,
+      },
+    }
   );
   await User.findOneAndUpdate(
-    { id: message.reply_to_message.from.id },
-    { $inc: { numberOfTaughtThings: 1 } }
+    {
+      id: message.reply_to_message.from.id,
+    },
+    {
+      $inc: {
+        numberOfTaughtThings: 1,
+      },
+    }
   );
 };
 
@@ -202,7 +282,14 @@ const onMemberLeft = async (message) => {
   const deletedUser = message.left_chat_member;
   if (deletedUser.is_bot) return;
 
-  await User.findOneAndUpdate({ id: deletedUser.id }, { isActive: false });
+  await User.findOneAndUpdate(
+    {
+      id: deletedUser.id,
+    },
+    {
+      isActive: false,
+    }
+  );
   bot.deleteMessage(message.chat.id, message.message_id);
   bot.sendMessage(
     process.env.ADMIN_USER_ID,
@@ -228,6 +315,7 @@ bot.onText(/\/stat (.+)/, (message, match) => {
 });
 bot.onText(/^\/bestStudent$/, getBestStudent);
 bot.onText(/^\/bestTeacher$/, getBestTeacher);
+bot.onText(/^\/learnfulDate$/, getLearnfulDate);
 bot.onText(/^\/stat$/, getStatistics);
 bot.onText(/^\+|⁺|＋|﹢$/, onLearnedNewThing);
 bot.on('new_chat_members', onNewMembersJoined);
