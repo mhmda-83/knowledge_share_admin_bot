13bee3570a0f4b9e386552298e20d19d12609c6f
Mohammad MohammadAlian <mralian.dev@gmail.com>
2020-06-04 16:55:23 +0430

    using mongodb query to find in active users and fix send message problems :)

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const moment = require('moment-jalaali');
 
 const User = require('./models/User');
 const bot = require('./bot');
@@ -16,27 +17,29 @@ app.get('/removeInactiveUsers', async (req, res) => {
   if (req.query.security_code != process.env.SECURITY_CODE) {
     return res.json({
       status: 'fail',
-      message: 'کد امنیتی اشتباست',
+      message: 'کد امنیتی اشتباه است',
     });
   }
-  const users = await User.find();
 
-  const yesterday = new Date().getTime() - 1000 * 60 * 60 * 24;
+  const users = await User.find({
+    isActive: true,
+    lastActivityDate: {
+      $lte: moment().utc().subtract(1, 'day').toDate(),
+    },
+  });
 
   users.forEach(async (user) => {
-    const lastActivityDate = new Date(user.lastActivityDate);
-    if (lastActivityDate.getTime() < yesterday) {
-      await bot.kickChatMember(process.env.GROUP_ID, user.id);
-      await bot.sendMessage(
-        message.chat.id,
-        `کاربر <a href="tg://user?id=${user.id}">${user.first_name}</a> ریمو شد`,
-        {
-          reply_to_message_id: message.message_id,
-          parse_mode: 'HTML',
-        }
-      );
-    }
+    await bot.kickChatMember(process.env.GROUP_ID, user.id);
+    await user.updateOne({ isActive: false });
+    await bot.sendMessage(
+      process.env.ADMIN_USER_ID,
+      `<a href="tg://user?id=${user.id}">کاربر</a> به دلیل عدم فعالیت ریمو شد`,
+      {
+        parse_mode: 'HTML',
+      }
+    );
   });
+
   res.json({
     status: 'success',
     message: 'عملیات با موفقیت انجام شد',
